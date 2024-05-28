f551c9fa0f63b334c7002c7907d533c08fdebe36
21mb358@esisalama.org <21mb358@esisalama.org>
2024-05-28 21:55:22 +0200

    add a file main

---
+++ app/src/main/java/cd/bmduka/race_tracker/MainActivity.kt
@@ -0,0 +1,17 @@
+package cd.bmduka.race_tracker
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import com.example.racetracker.ui.RaceTrackerApp
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            RaceTrackerApp()
+        }
+    }
+}
